37267e5ae0f7fa67d6cbf2027a32dcd9b8e24dd2
mahfujul07 <mahfujulhuq07@gmail.com>
2022-10-23 09:59:13 +0400

    yeah spooky

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAUmZtylTmztcyegO-zUQJwZZuTdnAZqM",
+  authDomain: "chat-app-mahfuz.firebaseapp.com",
+  databaseURL: "https://chat-app-mahfuz-default-rtdb.firebaseio.com",
+  projectId: "chat-app-mahfuz",
+  storageBucket: "chat-app-mahfuz.appspot.com",
+  messagingSenderId: "321103549997",
+  appId: "1:321103549997:web:d34659faa5be7b2c20caa4",
+  measurementId: "G-439HX5ZSEB"
+  };
+
+const app = firebase.apps.length ? firebase.app() : firebase.initializeApp(firebaseConfig);
+
+const auth = app.auth();
+const db = app.firestore();
+const storage = firebase.storage();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+
+export { auth, db, storage, timestamp }
